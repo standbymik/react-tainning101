827cdecfd9a6b8ca2c10975b608ad1a9ebed0f20
StandbyMik <standbypm4ever@gmail.com>
2019-01-23 16:24:32 +0700

    edit more

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,7 @@ import createReduxStore from '../src/utilities/CreateReduxStore'
 import withRedux from 'next-redux-wrapper'
 import App, { Container } from 'next/app'
 import { Provider } from 'react-redux'
-import '../static/css/main.css'
-import './login.css'
-import './global.css'
+//EEimport '../static/css/main.css'
 
 import ConnectedLayout from '../src/components/Layout'
 
@@ -16,18 +14,18 @@ const ReduxStore = (initialState, options) => createReduxStore(initialState, opt
 class MyApp extends App {
 
     static async getInitialProps({ Component, router, ctx }) {
-        
-        let pageProps = {}
+
+        const pageProps = {}
 
         if (Component.getInitialProps) {
-          pageProps = await Component.getInitialProps(ctx)
+            pageProps = await Component.getInitialProps(ctx)
         }
-    
-        return { pageProps }
+
+        return
     }
 
     render() {
-        const {Component, pageProps, store, router} = this.props
+        const { Component, pageProps, store, router } = this.props
 
         return (
             <Container>
---
+++ src/reducers/loginReducer.js
@@ -0,0 +1,9 @@
+
+export default function (state={}, action) {
+    switch (action.type) {
+        case 'SUCCESS':
+            return state
+        default :
+           return state
+    }
+}
