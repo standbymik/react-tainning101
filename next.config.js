ba6e9976adf0b771fb40f8d80e4814bbc27361cb
StandbyMik <standbypm4ever@gmail.com>
2019-01-23 16:51:42 +0700

    add more

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS({
-    webpack(config) {
+    webpack : function (config) {
+      const originalEntry = config.entry
+      config.entry = async () => {
+        const entries = await originalEntry()
+
+        if (entries['main.js'] && !entries['main.js'].includes('./client/polyfills.js')) {
+          entries['main.js'].unshift('./client/polyfills.js')
+        }
+
+        return entries
+      }
       return config
     }
 })
